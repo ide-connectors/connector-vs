ba6dcbf0b5bb93e331e88236d357ed94b3b750c0
Janusz Gorycki <jgorycki@atlassian.com>
2010-01-06 10:30:21 +0000

    PLVS-40 - Spike AnkhSVN Integration

--- plvs/plvs/util/Constants.cs
+++ plvs/plvs/util/Constants.cs
@@ -1,5 +1,6 @@
 ﻿namespace Atlassian.plvs.util {
     public static class Constants {
+
         public static string ERROR_CAPTION = "Error";
         public const string QUESTION_CAPTION = "Question";
         public static string USER_NOT_VALUDATED = "Warning! This field is not validated prior to sending to JIRA";
