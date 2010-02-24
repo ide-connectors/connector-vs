a1c8593337dc6988b6f2cdde91511d5534dab05d
Janusz Gorycki <jgorycki@atlassian.com>
2010-02-24 08:59:44 +0000

    PLVS-89 - switch off isue links if no JIRA server is selected

--- plvs/plvs/markers/vs2010/texttag/LinkUpdateWorkaround.cs
+++ plvs/plvs/markers/vs2010/texttag/LinkUpdateWorkaround.cs
@@ -5,6 +5,17 @@ using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
 
 namespace Atlassian.plvs.markers.vs2010.texttag {
+
+    /// <summary>
+    /// 
+    /// This class is required as a workaround for an issue described here:
+    /// http://social.msdn.microsoft.com/Forums/en-SG/vseditorprerelease/thread/0ca39bad-785d-474b-89b9-84981dee5534
+    /// 
+    /// Microsoft acknowledged the issue in their code. Unfortunately, 
+    /// they will not be able to provide the fix before RTM is launched
+    /// 
+    /// </summary>
+    
     [Export(typeof(IWpfTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
