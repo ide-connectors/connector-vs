a3917b607e68044f16a443b327c11806505ed335
Janusz Gorycki <jgorycki@atlassian.com>
2010-06-14 08:46:40 +0000

    PLVS-191 - issue links in editor not hilighted sometimes

--- plvs/plvs/eventsinks/TextBufferDataEventSink.cs
+++ plvs/plvs/eventsinks/TextBufferDataEventSink.cs
@@ -6,7 +6,10 @@ using Microsoft.VisualStudio.TextManager.Interop;
 
 namespace Atlassian.plvs.eventsinks {
     public sealed class TextBufferDataEventSink : IVsTextBufferDataEvents {
-        public IVsTextLines TextLines { get; set; }
+
+        private IVsTextLines textLines;
+
+        public IVsTextLines TextLines { get { return textLines; } set { textLines = value; callDocumentOpened(); } }
 
         public IConnectionPoint ConnectionPoint { get; set; }
 
@@ -17,14 +20,18 @@ namespace Atlassian.plvs.eventsinks {
         public int OnLoadCompleted(int fReload) {
             ConnectionPoint.Unadvise(Cookie);
 
+            callDocumentOpened();
+
+            return VSConstants.S_OK;
+        }
+
+        private void callDocumentOpened() {
             bool sharp = isCSharpOrCppOrC(TextLines);
             if (sharp || isVb(TextLines)) {
                 JiraEditorLinkManager.OnDocumentOpened(TextLines, sharp 
-                    ? JiraEditorLinkManager.BufferType.CSHARP_OR_C_OR_CPP
-                    : JiraEditorLinkManager.BufferType.VISUAL_BASIC);
+                                                                      ? JiraEditorLinkManager.BufferType.CSHARP_OR_C_OR_CPP
+                                                                      : JiraEditorLinkManager.BufferType.VISUAL_BASIC);
             }
-
-            return VSConstants.S_OK;
         }
 
         private static bool isCSharpOrCppOrC(IVsTextLines textLines) {
