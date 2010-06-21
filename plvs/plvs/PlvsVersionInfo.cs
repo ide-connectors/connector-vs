8266ba80828fce4ac47e9017a86e655a32e9d6d6
Janusz Gorycki <jgorycki@atlassian.com>
2010-06-21 08:30:20 +0000

    PLVS-197 - crash on startup when running VS without escalated privs with UAC on

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -2,9 +2,9 @@
       namespace Atlassian.plvs {
           public class PlvsVersionInfo {
               public static string Version { get { return "1.1.0"; } }
-              public static string BuildType { get { return "SNAPSHOT"; } }
-              public static string Stamp { get { return "20100618-1135"; } }
-              public const string VersionAndStamp = "1.1.0-20100618-1135";
+              public static string BuildType { get { return "FINAL"; } }
+              public static string Stamp { get { return "20100621-1030"; } }
+              public const string VersionAndStamp = "1.1.0-20100621-1030";
           }
       }
     
