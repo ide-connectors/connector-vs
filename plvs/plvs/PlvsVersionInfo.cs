07798ed29be3ab2c6c63e4b90cb0ac25515d62f6
Janusz Gorycki <jgorycki@atlassian.com>
2010-09-10 13:40:31 +0200

    -

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -3,8 +3,8 @@
           public class PlvsVersionInfo {
               public static string Version { get { return "1.3.0"; } }
               public static string BuildType { get { return "SNAPSHOT"; } }
-              public static string Stamp { get { return "20100910-1314"; } }
-              public const string VersionAndStamp = "1.3.0-20100910-1314";
+              public static string Stamp { get { return "20100910-1335"; } }
+              public const string VersionAndStamp = "1.3.0-20100910-1335";
           }
       }
     
