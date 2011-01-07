63f9dbdcc217770a9cc7047a604dfdd7c397f03a
Janusz Gorycki <jgorycki@atlassian.com>
2011-01-07 09:58:17 +0100

    copyright date updated in about box

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -3,8 +3,8 @@
           public class PlvsVersionInfo {
               public static string Version { get { return "1.3.0"; } }
               public static string BuildType { get { return "SNAPSHOT"; } }
-              public static string Stamp { get { return "20110103-1044"; } }
-              public const string VersionAndStamp = "1.3.0-20110103-1044";
+              public static string Stamp { get { return "20110107-0951"; } }
+              public const string VersionAndStamp = "1.3.0-20110107-0951";
           }
       }
     
