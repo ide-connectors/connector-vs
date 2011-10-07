a29d5c476682fac3fe11498977a3d0255bad2d34
Janusz Gorycki <jgorycki@atlassian.com>
2011-10-07 14:10:18 +0200

    elease notes URL fix

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -2,9 +2,9 @@
       namespace Atlassian.plvs {
           public class PlvsVersionInfo {
               public static string Version { get { return "1.3.3"; } }
-              public static string BuildType { get { return "SNAPSHOT"; } }
-              public static string Stamp { get { return "20111007-1102"; } }
-              public const string VersionAndStamp = "1.3.3-20111007-1102";
+              public static string BuildType { get { return "STABLE"; } }
+              public static string Stamp { get { return "20111007-1408"; } }
+              public const string VersionAndStamp = "1.3.3-20111007-1408";
           }
       }
     
