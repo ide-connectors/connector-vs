baa97cca747d11c5e1f4719b716ef9f43b443039
Janusz Gorycki <jgorycki@atlassian.com>
2012-04-20 12:28:13 +0200

    stupid .NET 3.5

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -1,10 +1,10 @@
 
       namespace Atlassian.plvs {
           public class PlvsVersionInfo {
-              public static string Version { get { return "1.3.5"; } }
-              public static string BuildType { get { return "STABLE"; } }
-              public static string Stamp { get { return "20120322-1507"; } }
-              public const string VersionAndStamp = "1.3.5-20120322-1507";
+              public static string Version { get { return "1.3.6"; } }
+              public static string BuildType { get { return "SNAPSHOT"; } }
+              public static string Stamp { get { return "20120420-1226"; } }
+              public const string VersionAndStamp = "1.3.6-20120420-1226";
           }
       }
     
