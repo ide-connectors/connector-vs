6b78060fe593e69d7f58260a0ea6cde14e928cda
Janusz Gorycki <jgorycki@atlassian.com>
2012-05-21 10:58:49 +0200

    IDESP-150

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -1,10 +1,10 @@
 
       namespace Atlassian.plvs {
           public class PlvsVersionInfo {
-              public static string Version { get { return "1.3.6"; } }
-              public static string BuildType { get { return "STABLE"; } }
-              public static string Stamp { get { return "20120420-1249"; } }
-              public const string VersionAndStamp = "1.3.6-20120420-1249";
+              public static string Version { get { return "1.3.7"; } }
+              public static string BuildType { get { return "SNAPSHOT"; } }
+              public static string Stamp { get { return "20120521-1055"; } }
+              public const string VersionAndStamp = "1.3.7-20120521-1055";
           }
       }
     
