222cf21728fa1fa95202fa2484222c470ba0946e
Janusz Gorycki <jgorycki@atlassian.com>
2013-03-26 12:16:32 +0100

    build

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -2,9 +2,9 @@
       namespace Atlassian.plvs {
           public class PlvsVersionInfo {
               public static string Version { get { return "1.3.7"; } }
-              public static string BuildType { get { return "SNAPSHOT"; } }
-              public static string Stamp { get { return "20130326-1144"; } }
-              public const string VersionAndStamp = "1.3.7-20130326-1144";
+              public static string BuildType { get { return "STABLE"; } }
+              public static string Stamp { get { return "20130326-1159"; } }
+              public const string VersionAndStamp = "1.3.7-20130326-1159";
           }
       }
     
