04618355304ce9091a97a20904661766b792b08b
Janusz Gorycki <jgorycki@atlassian.com>
2013-03-27 12:42:19 +0100

    1.3.8 snapshots

--- plvs/plvs/PlvsVersionInfo.cs
+++ plvs/plvs/PlvsVersionInfo.cs
@@ -3,8 +3,8 @@
           public class PlvsVersionInfo {
               public static string Version { get { return "1.3.7"; } }
               public static string BuildType { get { return "SNAPSHOT"; } }
-              public static string Stamp { get { return "20130327-1155"; } }
-              public const string VersionAndStamp = "1.3.7-20130327-1155";
+              public static string Stamp { get { return "20130327-1236"; } }
+              public const string VersionAndStamp = "1.3.7-20130327-1236";
           }
       }
     
